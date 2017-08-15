e7a2ebbfd73a9d05bab9b410948a5f91c8ebb279
insu <positiveinsu@gmail.com>
2017-08-15 13:39:44 -0500

    signup-complete 추가

--- frontend/src/app/signup/signup-complete.component.ts
+++ frontend/src/app/signup/signup-complete.component.ts
@@ -5,10 +5,31 @@ import {Component}        from "@angular/core";
 import {SignupService}    from "../_service/signup.service";
 @Component({
   selector: 'coims-signup-complete',
+  styleUrls: ['./signup-complete.component.css'],
   templateUrl: './signup-complete.component.html'
 })
 export class SignupCompleteComponent{
+
   constructor(private signupService: SignupService){
+    this.initProgressBar();
+    this.deleteConsultantObj();
+  }
+
+  private initProgressBar() {
     this.signupService.progressBarObj.currentStep = 4;
+  }
+
+  private deleteConsultantObj() {
+    console.log( this.signupService.consultantObj);
+    if( this.signupService.isNullConsultantObj){
+      this.signupService.deleteConsultantObj();
+    }
+  }
+
+  private goLoginPage(){
+    if( this.signupService.isNullConsultantObj){
+      this.signupService.deleteConsultantObj();
+    }
+    return '/login';
   }
 }
