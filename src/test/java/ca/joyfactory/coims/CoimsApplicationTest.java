86063de9f03d56b27956f9182f75160b1e5b3c52
insu <positiveinsu@gmail.com>
2017-09-04 20:51:42 -0500

    Junit Test 코드 디렉토리 변경

--- src/test/java/ca/joyfactory/coims/CoimsApplicationTest.java
+++ src/test/java/ca/joyfactory/coims/CoimsApplicationTest.java
@@ -1,16 +1,18 @@
-package ca.joyfactory;
+package ca.joyfactory.coims;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Created by Joinsu on 2017-09-04.
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CoimsApplicationTests {
+public class CoimsApplicationTest {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+    }
 }
